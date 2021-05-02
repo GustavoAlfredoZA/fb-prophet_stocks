c168dca1ee64d6b7d155020ce54d62e92920bf85
GustavoAlfredoZA <GustavoZA20@hotmail.com>
2021-05-01 20:39:41 -0500

    host

--- prophet_stocks/prophet_stocks/settings.py
+++ prophet_stocks/prophet_stocks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fjsqlvdc+hwc)p-_+v%)3#=sv48rmn2*m0v4-n@y7*o637st&j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fb-prophet-stocks.herokuapp.com']
 
 
 # Application definition
