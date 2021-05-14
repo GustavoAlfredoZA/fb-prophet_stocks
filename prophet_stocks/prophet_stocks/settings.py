1337e1b14775f165e91cc027cad59b5195908166
Gustavo <GustavoZA20@gmail.com>
2021-05-14 13:38:12 -0500

    settings

--- prophet_stocks/prophet_stocks/settings.py
+++ prophet_stocks/prophet_stocks/settings.py
@@ -14,14 +14,14 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEYS']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
