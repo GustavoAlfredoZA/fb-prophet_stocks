ae74e39e4c8da717555903d3a0953194dbff924a
Gustavo <GustavoZA20@gmail.com>
2021-05-14 13:29:32 -0500

    settings

--- prophet_stocks/prophet_stocks/settings.py
+++ prophet_stocks/prophet_stocks/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fjsqlvdc+hwc)p-_+v%)3#=sv48rmn2*m0v4-n@y7*o637st&j'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
