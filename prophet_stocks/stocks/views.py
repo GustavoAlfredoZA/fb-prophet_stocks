ad29d6d0f30115cdd64badfeb029ddd6fdaa8c40
GustavoAlfredoZA <GustavoZA20@hotmail.com>
2021-06-07 16:42:48 -0500

    date path

--- prophet_stocks/stocks/views.py
+++ prophet_stocks/stocks/views.py
@@ -7,7 +7,7 @@ from datetime import date
 def index(request):
     #latest_question_list = s
     #context = {'latest_question_list': latest_question_list}
-    f = open("date.txt", "r")
+    f = open("stocks/date.txt", "r")
     print(f)
     List = stocks.stockAnalisys.stockL
     args = {}
