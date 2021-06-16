d822bddff28a9c2d27a96e70299a93ced6f298c5
GustavoAlfredoZA <GustavoZA20@hotmail.com>
2021-06-15 22:52:45 -0500

    change name

--- prophet_stocks/stocks/TAnalysis.py
+++ prophet_stocks/stocks/TAnalysis.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import yahoo_fin.stock_info as si
 import pandas as pd
 
-def FA(ticker):
+def TA(ticker):
     stock = yf.Ticker(ticker)
 
     return stock.history(period="max")
