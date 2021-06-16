1dfd8e370e9b07dd2b777415a3a3027da39c2a9a
GustavoAlfredoZA <GustavoZA20@hotmail.com>
2021-06-15 21:27:08 -0500

    TickerList

--- prophet_stocks/stocks/FAnalysis.py
+++ prophet_stocks/stocks/FAnalysis.py
@@ -2,7 +2,8 @@ import yfinance as yf
 import yahoo_fin.stock_info as si
 import pandas as pd
 
-def FA(ticker):
-    stock = yf.Ticker(ticker)
-
-    return stock.history(period="max")
+def FA(stocks):
+    quoteTable = {}
+    for ticker in stocks:
+        quoteTable[ticker] = si.get_quote_table(ticker)
+    return quoteTable
